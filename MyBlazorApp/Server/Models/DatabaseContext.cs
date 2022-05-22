fa73a1648cca40023684b97d5fce9d4463707490
Mateo <Mateo@KORISNIK-PC>
2022-05-22 15:28:01 +0200

    Add project files.

---
+++ MyBlazorApp/Server/Models/DatabaseContext.cs
@@ -0,0 +1,33 @@
+﻿using MyBlazorApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBlazorApp.Server.Models
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<User> Users { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("userdetails");
+                entity.Property(e => e.Id).HasColumnName("Userid");
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

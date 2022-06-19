caddbe9ae2c60dd2ec448d1a2e2ecebd183b3361
Ratko Ćosić <dedalus123@gmail.com>
2022-06-19 21:00:24 +0200

    corrections

--- MyBlazorApp/Server/Entities/WorkTime.cs
+++ MyBlazorApp/Server/Entities/WorkTime.cs
@@ -1,16 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyBlazorApp.Server.Entities
 {
+    [Table("WorkTime")]
     public class WorkTime
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public int UserId { get; set; }
+
+        [Required]
         public DateOnly Day { get; set; }
+
+        [Required]
         public TimeOnly StartTime { get; set; }
+
+        [Required]
         public TimeOnly EndTime { get; set; }
+
+        [Required]
         public TimeSpan BreakTime { get; set; }
+
+        [MaxLength(255)]
+        [Unicode]
         public string? Notes { get; set; }
     }
 }

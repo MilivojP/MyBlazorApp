6cfb94c167015928ba516dc9943c6f072d4c13fb
Ratko Ćosić <dedalus123@gmail.com>
2022-07-11 16:35:27 +0200

    user id fixed

--- MyBlazorApp/Server/AutoMapperProfile.cs
+++ MyBlazorApp/Server/AutoMapperProfile.cs
@@ -22,16 +22,14 @@ namespace MyBlazorApp.Server
             // Vacation
             CreateMap<Vacation, VacationDto>();
             CreateMap<NewVacationDto, Vacation>()
-                .ForMember(x => x.Id, o => o.Ignore())
-                .ForMember(x => x.UserId, o => o.Ignore());
+                .ForMember(x => x.Id, o => o.Ignore());
             CreateMap<ExistingVacationDto, Vacation>();
             CreateMap<Vacation, ExistingVacationDto>();
 
             // WorkTime
             CreateMap<WorkTime, WorkTimeDto>();
             CreateMap<NewWorkTimeDto, WorkTime>()
-                .ForMember(x => x.Id, o => o.Ignore())
-                .ForMember(x => x.UserId, o => o.Ignore());
+                .ForMember(x => x.Id, o => o.Ignore());
             CreateMap<ExistingWorkTimeDto, WorkTime>();
             CreateMap<WorkTime, ExistingWorkTimeDto>();
 
@@ -52,8 +50,7 @@ namespace MyBlazorApp.Server
             //SickLeave:
             CreateMap<SickLeave, SickLeaveDto>();
             CreateMap<SickLeaveDto, SickLeave>()
-                .ForMember(x => x.Id, o => o.Ignore())
-                .ForMember(x => x.UserId, o => o.Ignore());
+                .ForMember(x => x.Id, o => o.Ignore());
         }
     }
 

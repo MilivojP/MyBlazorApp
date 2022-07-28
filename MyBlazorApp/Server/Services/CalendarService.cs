7a1c67648b21f10eb19d6693934065a2da9deda6
Mateo <Mateo@KORISNIK-PC>
2022-07-28 12:44:54 +0200

    Change CalendarService

--- MyBlazorApp/Server/Services/CalendarService.cs
+++ MyBlazorApp/Server/Services/CalendarService.cs
@@ -1,15 +1,41 @@
-﻿using MyBlazorApp.Server.Interfaces;
+﻿using MyBlazorApp.Server.Data;
+using MyBlazorApp.Server.Interfaces;
 
 namespace MyBlazorApp.Server.Services
 {
     public class CalendarService : ICalendarService
     {
-        public bool IsOverlapping(DateOnly dateFrom, DateOnly dateTo)
+        readonly DatabaseContext _dbContext;
+        
+
+        public bool IsOverlapping(DateOnly dateFrom, DateOnly dateTo )
         {
-            //TODO add dbContext
+            
             //TODO check WorkTime
+            if (_dbContext.WorkTimes.Any(x => x.Day >= dateFrom && x.Day < dateTo))
+            {
+               //return true;
+                IsOverlapping(dateFrom, dateTo);
+            }
+            else
+
+                throw new NotImplementedException();
+
             //TODO check SickLeave
+            if (_dbContext.SickLeaves.Any(x => x.StartDate >= dateFrom && x.EndDate <= dateTo))
+            {
+                IsOverlapping(dateFrom, dateTo);
+            }
+            else
+                
+            throw new NotImplementedException();
+
             //TODO check Vacation
+            if (_dbContext.Vacations.Any(x => x.DateFrom>= dateFrom && x.DateTo<= dateTo))
+            {
+                IsOverlapping(dateFrom, dateTo);
+            }
+
             throw new NotImplementedException();
         }
     }
